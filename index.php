e2720b1596c5d278d9bf631dba4efa24d027e7e2
Ryan Osten <ostenrj@gmail.com>
2020-04-12 13:49:06 -0400

    added redirect to index.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 require_once 'inc/quiz.php';
+
+$answers = [
+        $_SESSION['current_question']['firstIncorrectAnswer'],
+        $_SESSION['current_question']['secondIncorrectAnswer'],
+        $_SESSION['current_question']['correctAnswer'],
+];
+
+$orderedAnswers = [];
+
+do {
+    $rand = rand(0, 2);
+    if (!in_array($answers[$rand], $orderedAnswers)) {
+        array_push($orderedAnswers, $answers[$rand]);
+    }
+} while (count($orderedAnswers) < 3);
+
+
 ?>
 
 <html lang="en">
@@ -13,13 +30,14 @@ require_once 'inc/quiz.php';
 <body>
     <div class="container">
         <div id="quiz-box">
+            <span><?=$toast?></span>
             <p class="breadcrumbs">Question <?=count($_SESSION['questions_asked'])?> of <?= $num_questions?>?</p>
             <p class="quiz">What is <?=$_SESSION['current_question']['leftAdder']?> + <?=$_SESSION['current_question']['rightAdder']?>?</p>
-            <form action="inc/quiz.php" method="post">
+            <form action="index.php" method="post">
                 <input type="hidden" name="id" value="0" />
-                <input type="submit" class="btn" name="answer" value="<?=$_SESSION['current_question']['firstIncorrectAnswer']?>" />
-                <input type="submit" class="btn" name="answer" value="<?=$_SESSION['current_question']['secondIncorrectAnswer']?>" />
-                <input type="submit" class="btn" name="answer" value="<?=$_SESSION['current_question']['correctAnswer']?>"/>
+                <input type="submit" class="btn" name="answer" value="<?=$orderedAnswers[0]?>" />
+                <input type="submit" class="btn" name="answer" value="<?=$orderedAnswers[1]?>" />
+                <input type="submit" class="btn" name="answer" value="<?=$orderedAnswers[2]?>"/>
             </form>
         </div>
     </div>
