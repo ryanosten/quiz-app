e82802dd88783b9df3d53d05ec68c13b23c12bc0
Ryan Osten <ostenrj@gmail.com>
2020-04-19 11:47:03 -0400

    fixed bug with generateLeftAdder function

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -4,16 +4,16 @@
     //generates random answers within + or - 10 of correct answer, and ensure they aren't same as correct answer
     function generateFirstRandomAnswer($correct_answer) {
         do {
-            $randNum = rand(($correct_answer - 10), ($correct_answer + 10));
+            $randNum = rand(($correct_answer - 5), ($correct_answer + 5));
         } while ($randNum == $correct_answer);
 
         return $randNum;
     }
 
-    // generates random answers within + or - 10 of correct answer, and ensure they aren't same as correct answer or the first incorrect answer
+    // generates random answers within + or - 5 of correct answer, and ensure they aren't same as correct answer or the first incorrect answer
     function generateSecondRandomAnswer($correct_answer, $first_incorrect_answer) {
         do {
-            $randNum = rand(($correct_answer - 10), ($correct_answer + 10));
+            $randNum = rand(($correct_answer - 5), ($correct_answer + 5));
         } while ($randNum == $correct_answer OR $randNum == $first_incorrect_answer);
 
         return $randNum;
@@ -22,11 +22,16 @@
     //generates a random number to add as an adder and make sure that it doesnt exist in any other questions (so answer is unique)
     function generateAdder($question_set){
 
-        do {
-            $randNum = rand(0, 100);
-        } while(array_column($question_set, 'leftAdder') == $randNum ?: '0' OR array_column($question_set, 'rightAdder') == $randNum ?: '0');
+        if (count($question_set) == 0) {
+            return rand(0, 100);
+        } else {
 
-        return $randNum;
+            do {
+                $randNum = rand(0, 100);
+            } while (array_column($question_set, 'leftAdder') == $randNum OR array_column($question_set, 'rightAdder') == $randNum);
+
+            return $randNum;
+        }
     }
 
     //generates the entire question set for the quiz. Number of questions in quiz can be changed by change the number of loops
